4efc8f4ccbb50c1597411c35d88d50cd38b30719
Sıtkı Efe Kılınç <kilinc12efe@gmail.com>
2026-02-05 03:21:43 +0300

    fixed-Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -2,17 +2,25 @@ using Carter;
 using RAgentBackend.Shared.AI;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MobileAppPolicy", policy =>
+    {
+        policy.AllowAnyOrigin()   
+              .AllowAnyMethod()  
+              .AllowAnyHeader();  
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpClient<IAIClient, OllamaClient>();
-
 builder.Services.AddCarter();
 
 var app = builder.Build();
 
-
 app.UseHttpsRedirection();
-
+app.UseCors("MobileAppPolicy");
 
 app.MapCarter();
-
 app.Run();
--- server/obj/Debug/net9.0/RAgentBackend.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/RAgentBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RAgentBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb4235e494baa3b0c43d3ac35d57dc08d9926f84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1e9102d2ec797c1c43c77a0ee52ce25e98004d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RAgentBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RAgentBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
